f78edac095e4437230e944b38d7fa19bb539d656
dhenitgp12345 <44762412+dhenitgp12345@users.noreply.github.com>
2020-10-19 22:42:03 +0700

    kritik

---
+++ seani/app/src/main/java/com/staydev/file/Kritik.kt
@@ -0,0 +1,11 @@
+package com.staydev.file
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Kritik : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kritik)
+    }
+}
