be11c68a2b6f77f89b2d12afdd8003285ea25967
dhenitgp12345 <44762412+dhenitgp12345@users.noreply.github.com>
2020-10-19 21:11:50 +0700

    home

---
+++ seani/app/src/main/java/com/staydev/file/X.kt
@@ -0,0 +1,11 @@
+package com.staydev.file
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class X : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_x)
+    }
+}
