d4e4b9d41c202c6220e751befe5d5a2e266c53ba
dhenitgp12345 <44762412+dhenitgp12345@users.noreply.github.com>
2020-10-25 21:09:46 +0700

    desain

---
+++ seani/app/src/main/java/com/staydev/file/Chat.kt
@@ -0,0 +1,11 @@
+package com.staydev.file
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
