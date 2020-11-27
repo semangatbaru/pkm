9234c3aa3fb8dcdb4e8439115c8b814adacd1475
dhenitgp12345 <44762412+dhenitgp12345@users.noreply.github.com>
2020-11-27 05:53:44 -0800

    register

---
+++ seani/app/src/main/java/com/staydev/seani/Coba.kt
@@ -0,0 +1,27 @@
+package com.staydev.seani
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.staydev.seani.ui.dashboard.DashboardViewModel
+
+class Coba : Fragment() {
+
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.activity_register, container, false)
+
+
+        return root
+    }
+}
