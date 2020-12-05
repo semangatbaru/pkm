c23a693755d0b23823c5fb1591566b5ef20e64b9
dhenitgp12345 <44762412+dhenitgp12345@users.noreply.github.com>
2020-12-05 05:42:28 -0800

    kernjang

---
+++ seani/app/src/main/java/com/staydev/seani/activity_keranjang.kt
@@ -0,0 +1,11 @@
+package com.staydev.seani
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_keranjang : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keranjang)
+    }
+}
